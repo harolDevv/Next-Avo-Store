af907eab28c2b3b7cdce6a82cce633bbea81e3f0
Harold <harold.tenorio@epn.edu.ec>
2022-08-21 23:07:21 -0500

    next.config

--- next.config.js
+++ next.config.js
@@ -5,21 +5,3 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
-
-const nextConfig = {
-  webpack: function(config){
-  config.module.rules.push({
-      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-      use: {
-      loader: 'url-loader',
-          options: {
-          limit: 100000,
-          name: '[name].[ext]'
-      }}
-  })
-  return config
-  },
-  env: {
-      MONGO_URI: "your uri"
-  }
-}
